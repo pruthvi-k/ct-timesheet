1d17e0890959d983c1d01e2aa7d461824d10afb8
Burboz <502698333@logon.ds.ge.com>
2018-08-23 15:55:07 +0530

    datePicker changes

--- src/pages/attendance/attendance.ts
+++ src/pages/attendance/attendance.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// import { DatePicker } from '@ionic-native/date-picker'
 
 /**
  * Generated class for the AttendancePage page.
@@ -14,12 +15,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'attendance.html',
 })
 export class AttendancePage {
-
+  items = [
+    'Present',
+    'Personal Leave',
+    'Work from home',
+  ];
+  attendanceOptions = [
+    {
+      "key" : "Present",
+      "value" : "P"
+    },
+    {
+      "key" : "Personal Leave",
+      "value" : "PL"
+    },
+    {
+      "key" : "Work from home",
+      "value" : "WFH"
+    }
+];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.today = new Date().toISOString();
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AttendancePage');
   }
-
+  // calendar(){
+  //   this.datePicker.show({
+  //     date: new Date(),
+  //     mode: 'date',
+  //     androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+  //   }).then(
+  //     date => console.log('Got date: ', date),
+  //     err => console.log('Error occurred while getting date: ', err)
+  //   );
+  // }
 }
